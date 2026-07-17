5fa1b498def4f32c98513dc87aa06df9ebced87b
vishal <outliervishal@gmail.com>
2026-07-17 10:28:58 +0530

    deleted unused files

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,10 @@
 import express from "express";
-import dotenv from "dotenv";
-import authRoutes from  "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js"
-dotenv.config();
+import "dotenv/config";
+
+
 const app = express();
-const PORT = process.env.PORT|| 3000;
-app.use("/api/auth",authRoutes);
-app.use("/api/messages", messageRoutes);
+const PORT = process.env.PORT;
+
+
 app.listen(PORT,()=> console.log("Server running on port: " + PORT));
 

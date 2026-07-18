5a7a6c984f9d69e332792f624ffb95d4114291d4
vishal <outliervishal@gmail.com>
2026-07-18 18:49:22 +0530

    add cron.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,6 +27,9 @@ if (fs.existsSync(publicDir)) {
 app.listen(PORT,()=> {
     connectDB();
     console.log("Server is up and running on port:",PORT);
+     if (process.env.NODE_ENV === "production") {
+      job.start();
+     }
 
 });
 

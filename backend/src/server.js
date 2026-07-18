37bb62755c2518276c982fb4f485193ddcd4757e
vishal <outliervishal@gmail.com>
2026-07-18 19:03:39 +0530

    update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,9 +27,7 @@ if (fs.existsSync(publicDir)) {
 app.listen(PORT,()=> {
     connectDB();
     console.log("Server is up and running on port:",PORT);
-     if (process.env.NODE_ENV === "production") {
-      job.start();
-     }
+     if (process.env.NODE_ENV === "production") job.start();
 
 });
 
